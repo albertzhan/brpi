4d35f9e598c186684ea34a1f0dc7e9016dcd846c
You Zhou <youzhouphoenix@hotmail.com>
2018-05-19 13:44:37 -0400

    Adding name to a string

--- clientStuff/clientMain.py
+++ clientStuff/clientMain.py
@@ -1,12 +1,16 @@
 import cv2, keyboardDisplay
 from lomond import websocket
 from pygame import *
+init()
+font.init()
 websocket = websocket.WebSocket("ws://localhost:8888/brpi")
 running = True
-screen = display.set_mode((720,480))
+screen = display.set_mode((720,515))
 cam = cv2.VideoCapture(0)
 clockity = time.Clock()
 oldKey = ""
+name = ""
+timesnr = font.Font("../BurbankBigCondensed-Bold.otf",35)
 while running: #this will keep trying to connect the websocket if the websocket dc's
     for ws in websocket.connect(poll=0): # this is essentially the new event loop, it will stop running if the connection is broken
         if ws.name!='poll':
@@ -25,10 +29,17 @@ while running: #this will keep trying to connect the websocket if the websocket 
         keyPress = keyboardDisplay.keyboard(screen, 50, 0, 280,mx,my,mb)
         if keyPress!=None and keyPress!=oldKey:
             print(keyPress)
+            if keyPress == "backspace":
+                if len(name) > 0:
+                    name = name[:len(name)-1]
+            else:
+                name += keyPress
             oldKey = keyPress
         elif keyPress == None:
             oldKey = ''
         display.flip()
         clockity.tick(30)
+        draw.rect(screen,(180,180,180),(0,480,720,35))
+        screen.blit(timesnr.render(name,True,(0,0,0)),(0,480))
 cam.release()
 quit()
