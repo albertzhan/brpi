cfd82d6fccb561001d83d7cc6bca6fa30204a552
Dumfing <dumpling.d.m@gmail.com>
2018-05-19 11:57:31 -0400

    stuff

--- clientMain.py
+++ clientMain.py
@@ -9,4 +9,6 @@ while running:
         if e.type == QUIT:
             running = False
     mx, my = mouse.get_pos()
+    stuff, frame = cam.read()
+    pySurf = transform.rotate(surfarray.make_surface(cv2.COLOR_BGR2RGB(frame)),-90)
 quit()
--- serverMain.py
+++ serverMain.py
@@ -1,13 +1,16 @@
 import tornado.websocket, tornado.ioloop, tornado.web
+import client
+clients = {}
+stage = "fill"
 class WebsocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
-        print('socket opened')
+        clients[self] = client.Client(self)
+        self.write_message((0,stage))
     def on_message(self, message):
-        self.write_message("echo! "+message)
+        pass
     def on_close(self):
-        print("socket closed")
+        del clients[self]
 if __name__ == "__main__":
     app = tornado.web.Application(['r/brpi', WebsocketHandler])
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
-    
