9c5057e1fb23423c699a5b2ed8d330731f98cdc5
Dumfing <dumpling.d.m@gmail.com>
2018-05-19 12:14:05 -0400

    periodic callback

--- serverMain.py
+++ serverMain.py
@@ -2,7 +2,9 @@ import tornado.websocket, tornado.ioloop, tornado.web
 import client
 clients = {}
 stage = "fill"
-class WebsocketHandler(tornado.websocket.WebSocketHandler):
+def periodic_callback():
+    print("hi")
+class wsHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         clients[self] = client.Client(self)
         self.write_message((0,stage))
@@ -11,6 +13,8 @@ class WebsocketHandler(tornado.websocket.WebSocketHandler):
     def on_close(self):
         del clients[self]
 if __name__ == "__main__":
-    app = tornado.web.Application(['r/brpi', WebsocketHandler])
+    app = tornado.web.Application([(r'/brpi', wsHandler)])
     app.listen(8888)
+    tornado.ioloop.PeriodicCallback(periodic_callback,1000).start()
+    print('callback started')
     tornado.ioloop.IOLoop.current().start()
