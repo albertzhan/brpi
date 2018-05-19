f68df83a820e3eea3c3338f4ba1b368c227994d4
Dumfing <dumpling.d.m@gmail.com>
2018-05-19 13:28:26 -0400

    add stages

--- clientStuff/clientMain.py
+++ clientStuff/clientMain.py
@@ -7,6 +7,7 @@ screen = display.set_mode((720,480))
 cam = cv2.VideoCapture(0)
 clockity = time.Clock()
 oldKey = ""
+stage = 1
 while running: #this will keep trying to connect the websocket if the websocket dc's
     for ws in websocket.connect(poll=0): # this is essentially the new event loop, it will stop running if the connection is broken
         if ws.name!='poll':
@@ -17,17 +18,24 @@ while running: #this will keep trying to connect the websocket if the websocket 
                 websocket.close()
         mx, my = mouse.get_pos()
         mb = mouse.get_pressed()[0]
-        stuff, frame = cam.read()
-        pySurf = transform.rotate(surfarray.make_surface(
-            cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        ),-90)
-        screen.blit(pySurf, (0,0))
-        keyPress = keyboardDisplay.keyboard(screen, 50, 0, 280,mx,my,mb)
-        if keyPress!=None and keyPress!=oldKey:
-            print(keyPress)
-            oldKey = keyPress
-        elif keyPress == None:
-            oldKey = ''
+        if stage == 1:
+            pass
+        elif stage == 2:
+            pass
+        elif stage == 3:
+            pass
+        elif stage == 4:
+            stuff, frame = cam.read()
+            pySurf = transform.rotate(surfarray.make_surface(
+                cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            ), -90)
+            screen.blit(pySurf, (0, 0))
+            keyPress = keyboardDisplay.keyboard(screen, 50, 0, 280, mx, my, mb)
+            if keyPress != None and keyPress != oldKey:
+                print(keyPress)
+                oldKey = keyPress
+            elif keyPress == None:
+                oldKey = ''
         display.flip()
         clockity.tick(30)
 cam.release()
