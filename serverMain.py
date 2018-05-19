c4a9a2eb2344c970202df9d4074afa04dbc7f7e9
Dumfing <dumpling.d.m@gmail.com>
2018-05-19 10:47:13 -0400

    basic SpaceBodge Server

--- serverMain.py
+++ serverMain.py
@@ -1,5 +1,13 @@
-import tornado.websocket, tornado.ioloop
+import tornado.websocket, tornado.ioloop, tornado.web
 class WebsocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
-        pass
+        print('socket opened')
+    def on_message(self, message):
+        self.write_message("echo! "+message)
+    def on_close(self):
+        print("socket closed")
+if __name__ == "__main__":
+    app = tornado.web.Application(['r/brpi', WebsocketHandler])
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
     
